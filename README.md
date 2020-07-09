6f5424c679619c68f9bd97023012f5c81cf194a3
Mua N. Laurent <muarachmann@gmail.com>
2020-07-10 00:51:48 +0100

    :sparkles: Get all activities within month
    Upgraded deps & scraps all activities within a month

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# gsoc web_scrapping
-A program to scrap gsoc website and notify
+# GSoC Timeline Scrapper.
+A program to scrap gsoc website and notify students/mentors on the current stage they are in the program.
+
+By default, it shows the timeline of the current month.
+
+### Usage
+
+```bash
+python3 -m pip install -r requirements.txt
+```
+
+```bash
+python3 main.py
+```
+
+Works with the the - [Cameroon GSoC bot](https://github.com/ivange94/cgsbot) to keep students posted when ever there is an event
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 from datetime import datetime
 
 # declare variables
-rows = []
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 gsoc_timeline_url = requests.get("https://developers.google.com/open-source/gsoc/timeline")
 
@@ -12,7 +11,7 @@ soup = BeautifulSoup(gsoc_timeline_url.content, 'html5lib')
 # get GSoC timeline table
 table = soup.find('table')
 
-#get all trs in the table body
+# get all trs in the table body
 trs = table.tbody.findAll('tr')
 
 # format date and and time according to GSoC specifications
@@ -21,13 +20,21 @@ def format_date(timeline):
 
 
 def main():
+    results = [];
+    rows = []
     for tr in trs:
         rows.append([col.text for col in tr.findAll('td')])
 
     for row in rows[1:]:
         formatted_date = format_date(row[0])
         if formatted_date[0] in months and formatted_date[0] == datetime.now().strftime("%B"):
-            return row[0] + " - " + row[1]
+            results.append(row[0] + " - " + row[1])
+    
+    if len(results) < 0:
+        print("No timeline available at this time")
+    else:
+        for result in results:
+            print(result + "\n")
 
 if __name__ == '__main__':
-    print (main())
+    main()
